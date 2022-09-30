dedd347d09a77bfc68bcb23c1966ff3490ba861f
boltaevt <76846248+boltaevt@users.noreply.github.com>
2022-09-30 19:14:00 +0300

    Initial commit

---
+++ src/YRecord.java
@@ -0,0 +1,16 @@
+public class YRecord {
+
+    int month;
+    int amount;
+    boolean isExpense;
+
+    public YRecord (int month, int amount, boolean isExpense) {
+        this.month = month;
+        this.amount = amount;
+        this.isExpense = isExpense;
+    }
+
+}
+
+
+
